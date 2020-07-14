9ac1bdc88c29c51a32f7947820ce2b17eb8fa40c
PedAlava <53833631+PedAlava@users.noreply.github.com>
2020-07-14 17:28:27 -0500

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -6,7 +6,10 @@ import os
 from flask import Flask, request
 from chatterbot.response_selection import get_random_response #Para el adaptador lógico de respuesta aleatoria
 
+import aprendizajes
+
 chatbot = ChatBot("Skynet5_bot")
+
 
 chatbot = ChatBot(
     'Terminal',sqlite3storage_adapter='chatterbot.storage.SQLStorageAdapter',
@@ -42,32 +45,17 @@ trainer.train([ #Comentar este fragmento después de la primera ejecución exito
 
 trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
 'Hola',
-'Hola como estas en que te puedo ayudar'
+'Holis'
 ])
 trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
 'Buenos dias',
-'Buenos dias como estas en que te puedo ayudar'
-])
-
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'informacion',
-'Sobre que articulo necesita informacion'
-])
-
-
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'video',
-'No tenemos tarjetas de video por el momento'
-])
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'tarjeta de video',
-'No tenemos tarjetas de video por el momento'
+'Buenos dias como estas'
 ])
 
 
 trainer.train("chatterbot.corpus.spanish") #Comentar este fragmento después de la primera ejecución exitosa del programa
-#TOKEN = "1350904905:AAHWuHKc09pz2N_iq7R8Z6s3T0S2x_obM_M"
-#URL = "https://api.telegram.org/bot" + TOKEN + "/"
+TOKEN = "1009735735:AAE1aqiXB0KZKA2Bkz8iroC8pvvSP0Az1p0"
+URL = "https://api.telegram.org/bot" + TOKEN + "/"
 
 
 trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
@@ -75,83 +63,52 @@ trainer.train([ #Comentar este fragmento después de la primera ejecución exito
 'Mi creador es Pedro Alexander Alava Gil'
 ])
 
+
+
 def aprenderTodo(lista):
   trainer.train([lista[0],lista[1]])
 
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'necesito informacion',
-'Sobre que articulo necesita informacion tenemos por el momento Laptops y Celulare'
-])
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'Puedes darme informacion por favor',
-'Sobre que articulo necesitas informacion tenemos por el momento Laptops y celulares'
-])
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'necesito una tarjeta de video',
-'Por el momento no tenemos tarjetas de video solo contamos con laptops, celulares y productos novedosos que te pueden interesar si quiere te puedo redirigir a la pagina de nuestros <a href="{{ url_for("store") }}"> productos </a> '
-])
+def enviar_mensaje(idchat, texto):
+  requests.get(URL + "sendMessage?text=" + texto + "&chat_id=" + str(idchat))
 
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'necesito un dashboard',
-'Por el momento no tenemos dashboards solo contamos con laptops, celulares y productos novedosos que te pueden interesar si quiere te puedo redirigir a la pagina de nuestros <a href="{{ url_for("store") }}"> productos </a> '
-])
 
-trainer.train([ #Comentar este fragmento después de la primera ejecución exitosa del programa
-'quiero informacion de una tarjeta de video',
-'Por el momento no tenemos tarjetas de video solo contamos con laptops, celulares y productos novedosos que te pueden interesar si quiere te puedo redirigir a la pagina de nuestros <a href="{{ url_for("store") }}"> productos </a> '
-])
-l = ['una laptop','tenemos laptops de marcas Asus y Dell por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("laps") }}"> laptops </a>']
-l1 = ['laptop','tenemos laptops de marcas Asus y Dell por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("laps") }}"> laptops </a>']
-l2 = ['laptops','tenemos laptops de marcas Asus y Dell por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("laps") }}"> laptops </a>']
-l3 = ['celulares','tenemos celulares de marcas Xiamoi y Huawei por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l4 = ['celular','tenemos celulares de marcas Xiamoi y Huawei por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l5 = ['un celular','tenemos celulares de marcas Xiamoi y Huawei por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l6 = ['productos novedosos','tenemos algunos productos novedosos como reloj con kits medicos, cartera digital, etc por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l7 = ['quiero informacion de una laptop','tenemos laptops de marcas Asus y Dell por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("laps") }}"> laptops </a>']
-l8 = ['tienen laptops asus','Si tenemos laptops de marca Asus por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("laps") }}"> laptops </a>']
-l9 = ['tienen laptops dell','Si tenemos laptops de marca Dell por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("laps") }}"> laptops </a>']
-l10 = ['tienen celulares xiamoi','Si tenemos celulares de marca Xiamoi por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l11 = ['tienen celulares huawei','Si tenemos celulares de marca Huawei por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l12 = ['quiero informacion de un celular','tenemos celulares de marcas Xiamoi y Huawei por el momento si quieres te puedo redirigir a la pagina de <a href="{{ url_for("cells") }}"> Celulares </a>']
-l13 = ['que ofertas tienen','tenemos algunos ofertas en laptops y celulares si quieres te puedo redirigir a la pagina de nuestros <a href="{{ url_for("store") }}">productos </a> ']
-l14 = ['pago','aceptamos efectivo o tarjeta de credito']
-l15 = ['que tipo de pago aceptan','aceptamos efectivo o tarjeta de credito']
-l16 = ['metodo de pago','aceptamos efectivo o tarjeta de credito']
-l17 = ['que metodo de pago','aceptamos efectivo o tarjeta de credito']
-
-aprenderTodo(l)
-aprenderTodo(l1)
-aprenderTodo(l2)
-aprenderTodo(l3)
-aprenderTodo(l4)
-aprenderTodo(l5)
-aprenderTodo(l6)
-aprenderTodo(l7)
-aprenderTodo(l8)
-aprenderTodo(l9)
-aprenderTodo(l10)
-aprenderTodo(l11)
-aprenderTodo(l12)
-aprenderTodo(l13)
-aprenderTodo(l14)
-aprenderTodo(l15)
-aprenderTodo(l16)
-aprenderTodo(l17)
-#def enviar_mensaje(idchat, texto):
-    #Llamar el metodo sendMessage del bot, passando el texto y la id del chat
-    #requests.get(URL + "sendMessage?text=" + texto + "&chat_id=" + str(idchat))
-
-def respon(message):
-  message = message.lower()
-  respuesta = chatbot.get_response(message)
-  c = float(respuesta.confidence)
-  val = 0.5
-  if c > val:
-    texto_respuesta = str(respuesta)
-    c = 0
+def respons(id_chat,message):
+  aprendi=[]
+  lista = aprendizajes.obtener()
+  message_cloud = lista[0]
+  id_cloud = lista[1]
+  if message_cloud == "no":
+    message = message.lower()
+    response = chatbot.get_response(message)
+    c = float(response.confidence)
+    val = 0.5
+    if c > val:
+      texto_respuesta = str(response)
+      c = 0
+      n = int(id_cloud)
+      n += 1
+      aprendizajes.guardar(n,texto_respuesta,"bot","respuesta")
+    else:
+      texto_respuesta ="nose la respuesta enseñame por favor"
+      if response == "nose la respuesta enseñame por favor":
+        n = int(id_cloud)
+        n += 1
+        aprendizajes.guardar(n,texto_respuesta,"bot","respuesta")
+        enviar_mensaje(id,texto_respuesta)
+        c = 0
+  elif message_cloud == "aprendido":
+    texto_respuesta ="Cambiemos de tema por favor"
   else:
-    texto_respuesta ="No se la respuesta del mensaje"
-    c = 0
+    
+    aprendi.append(message_cloud)
+    aprendi.append(message)
+    aprenderTodo(aprendi)
+    n = int(id_cloud)
+    n += 1
+    aprendizajes.guardar(n,"Gracias por enseñarme","bot","respuesta")
+    texto_respuesta = "Gracias por enseñarme"
+  
   return texto_respuesta
+
 
 
